7986280c0c8d9607f845422bbe6c3962e5951913
prathmesh-mhatre <prathmesh.mhatre99@gmail.com>
2024-05-11 23:40:28 +0530

    SETUP

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Snap = () => {
+  return (
+    <section className="bg-white">
+      <div className="max-w-6xl max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8 bg-white text-black">
+        <div className="" >
+          <div className="left card-shadow rounded">Display Picture</div>
+          <div className="right">
+            <div className="top">
+              <div className="name card-shadow rounded">name</div>
+              <div className="experiance card-shadow rounded">Experiance</div>
+              <div className="location card-shadow rounded">location</div>
+              <div className="avatar card-shadow rounded">avatar</div>
+            </div>
+            <div className="about card-shadow rounded">about</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Snap;
